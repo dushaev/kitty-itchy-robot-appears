0cfbb8cb7374da8b77f3ea2f7f63d88905151450
K <avd@ya.ru>
2025-04-22 15:22:41 +0300

    Update project_beacon.ino
    interrupt for max speed

--- project_beacon.ino
+++ project_beacon.ino
@@ -1,18 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-#include <VolAnalyzer.h>
+#include <Ticker.h>
 
-#define UDP_PORT 4210  // Порт для UDP синхронизации
+#define TIMER_INTERVAL 5  // Минимальный стабильный интервал в микросекундах
+
+volatile uint32_t counter = 0;
+Ticker timer;
+
 #define SOUND_PIN D5   //Пин, к которому подсоединен пьезодинамик
-#define PIN_SENSOR D1  // Пин, к которому присоединен датчик вибрации
 #define PIN_MIC A0     // Пин, к которому присоединен микрофон
-
-#define SOUND_SPEED 343000  // Скорость звука в мм/с
 
 uint8_t masterAddress[] = { 0x14, 0x2B, 0x2F, 0xC5, 0x5F, 0x44 };  // MAC-адрес ESP32
 
 #define SET_STATUS_IDLE 8   //статус бездействие
-#define WAIT_FOR_SIGNAL 1   //ожидание звука
+#define WAIT_FOR_SIGNAL 7   //ожидание звука
 #define DO_MAKE_SOUND 2     //издать звук
 #define DO_UPDATE_DELAY 3   //обновить расхожение времени в микросекундах с сервером
 #define NEED_RECALIBRATE 5  //маяк был передвинут
@@ -34,12 +35,25 @@ struct __attribute__((packed)) TimeSyncPacket {
 };
 
 SlaveData myData;
-VolAnalyzer analyzer(PIN_MIC);
-int device_status = SET_STATUS_IDLE;
+int device_status = NEED_RECALIBRATE;
 uint32_t timeOffset = 0;  // Смещение времени относительно ESP32
+uint32_t s;
+uint32_t max_noice;
+uint32_t calibration_time;
+uint32_t time_fixed;
 
+void IRAM_ATTR timer_isr() {
+  if (device_status == WAIT_FOR_SIGNAL) {
+    time_fixed = micros();
+    s = analogRead(PIN_MIC);  // Быстрая операция в прерывании    
+    if (s > max_noice + 50) {      
+      device_status = DO_UPDATE_DELAY;
+    }
+  }
+}
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  system_update_cpu_freq(160);
   WiFi.mode(WIFI_AP_STA);
   WiFi.disconnect();
   if (esp_now_init() != 0) {
@@ -49,91 +63,50 @@ void setup() {
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
 
   esp_now_register_recv_cb(OnDataRecv);
-  esp_now_register_send_cb(OnDataSent);
 
   myData.id = ESP.getChipId();  // Уникальный ID для каждого устройства
-  Serial.print("my id=");
-  Serial.println(myData.id);
-  pinMode(PIN_SENSOR, INPUT);  //переводим пин в режим получения информации
   pinMode(SOUND_PIN, OUTPUT);  //переводим пин в режим отправки информации
-
-  analyzer.setVolK(20);  //настройка параметров микрофона
-  analyzer.setTrsh(10);
-  analyzer.setVolMin(10);
-  analyzer.setVolMax(100);
-  analyzer.setDt(10);
-  analyzer.setWindow(10);
 
   //отправим на сервер сигнал о готовности
   myData.type = NEED_RECALIBRATE;
   esp_now_send(masterAddress, (uint8_t *)&myData, sizeof(myData));
+
+  digitalWrite(SOUND_PIN, HIGH);  //издаем звук
+  delay(100);                     //ожидание 100 мс
+  digitalWrite(SOUND_PIN, LOW);   //выключаем звук
+  calibration_time = millis();
+  timer1_attachInterrupt(timer_isr);
+  timer1_enable(TIM_DIV1, TIM_EDGE, TIM_LOOP);
+  timer1_write(clockCyclesPerMicrosecond() * TIMER_INTERVAL);
 }
 
 void loop() {
-  if (device_status == WAIT_FOR_SIGNAL && detectSound()) {
+  if (device_status == DO_UPDATE_DELAY) {
     device_status = SET_STATUS_IDLE;
-    myData.timestamp = micros() + timeOffset;  // Корректировка времени с учетом смещения
+    myData.timestamp = time_fixed + timeOffset;  // Корректировка времени с учетом смещения
     myData.type = WAIT_FOR_SIGNAL;
-    delay(100);
+    WiFi.mode(WIFI_AP_STA);
     esp_now_send(masterAddress, (uint8_t *)&myData, sizeof(myData));
-    delay(100);
   }
-}
-
-bool detectSound() {
-  if (analyzer.tick() && analyzer.getRaw() > 3) {
-    Serial.println("Sound event!");
-    return true;
+  if (device_status == NEED_RECALIBRATE && calibration_time + 2000 > millis()) {
+    s = analogRead(PIN_MIC);
+    if (s > max_noice) {
+      max_noice = s;
+    }
   }
-  return false;
-}
-
-void OnDataSent(uint8_t *mac, uint8_t status) {
-  Serial.print(F("Данные получены от "));
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-           mac[0], mac[1], mac[2],
-           mac[3], mac[4], mac[5]);
-
-  Serial.println(macStr);
-  Serial.print(F("status="));
-  Serial.println(status);
 }
 
 void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
-  Serial.print(F("Данные получены от "));
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-           mac[0], mac[1], mac[2],
-           mac[3], mac[4], mac[5]);
-
-  Serial.println(macStr);
-
   if (len == sizeof(TimeSyncPacket)) {
     // Обработка синхронизации времени
-    TimeSyncPacket *sync = (TimeSyncPacket *)data;
-    uint32_t local_time = micros();
-    timeOffset = sync->master_time - local_time;
-    Serial.print(F("Обработка синхронизации времени"));
-    Serial.print(F("timeOffset="));
-    Serial.println(timeOffset);
+    TimeSyncPacket *sync = (TimeSyncPacket *)incomingData;
+    timeOffset = sync->master_time - micros();
   } else if (len == sizeof(BeaconCommand)) {
     BeaconCommand command;
     memcpy(&command, incomingData, sizeof(command));
-    Serial.print(F("Тип="));
-    Serial.println(command.command);
-    if (command.command == DO_MAKE_SOUND) {
-      device_status = SET_STATUS_IDLE;
-      delay(1000);
-      myData.timestamp = micros() + timeOffset;  // Запись времени издания звука + корректировка
-      digitalWrite(SOUND_PIN, HIGH);             //издаем звук
-      delay(100);                                //ожидание 100 мс
-      digitalWrite(SOUND_PIN, LOW);              //выключаем звук
-      myData.type = DO_MAKE_SOUND;               //этот маяк издал звук
-      esp_now_send(masterAddress, (uint8_t *)&myData, sizeof(myData));
-    }
     if (command.command == WAIT_FOR_SIGNAL) {
       device_status = WAIT_FOR_SIGNAL;
+      WiFi.mode(WIFI_OFF);
     }
     if (command.command == SET_STATUS_IDLE) {
       device_status = SET_STATUS_IDLE;
